e3ab7a77824d194fbfde4b6db2ad0e9909bc2640
yangzhen <yangzhen>
2018-08-22 09:54:05 +0800

    增加判断文件夹是否为空

--- src/common/node/nodedemo/controller/router.js
+++ src/common/node/nodedemo/controller/router.js
@@ -1,78 +1,88 @@
 var fileModel = require('../models/file.js');
-var formidable =  require('formidable');
+var formidable = require('formidable');
 var fs = require("fs");
 var path = require('path')
 
-exports.showIndex = function(req,res,next) {
+exports.showIndex = function (req, res, next) {
     // node思想，在获取完成全部的文件夹以后，调用回调callback，拿到全部数据进行render
-    fileModel.getPicsFolder(function(err,allPicArr){
-        if(err){
+    fileModel.getPicsFolder(function (err, allPicArr) {
+        if (err) {
             // res.render('err');
             next()
             return;
         }
-        res.render('index',{
-            ablums:allPicArr
-        })
+        // 对相册是否含有文件做处理
+        var allPicsData = [];
+        fileModel.perFolderHas(allPicArr, function (isHasHolder) {
+            allPicsData = isHasHolder;
+            res.render('index', {
+                ablums: allPicsData
+            })
+        });
     })
 }
 
-exports.showAlbums  = function(req,res,next) {
+exports.showAlbums = function (req, res, next) {
     // 文件图片名称
     var albumsNames = req.params.albumsNames;
-    fileModel.getAllPics(albumsNames,function(err,allPics){
-        if(err){
+    fileModel.getAllPics(albumsNames, function (err, allPics) {
+        if (err) {
             // res.render('err');
             next();
             return;
         }
-        res.render('pics',{
-            albumsNames:albumsNames,
-            pics:allPics
+        res.render('pics', {
+            albumsNames: albumsNames,
+            pics: allPics
         })
     })
 }
 
 // 上传页面路由
-exports.showUpload = function(req,res) {
-    fileModel.getPicsFolder(function(err,allPicArr){
-        if(err){
+exports.showUpload = function (req, res) {
+    fileModel.getPicsFolder(function (err, allPicArr) {
+        if (err) {
             next();
             return;
         }
-        res.render('upload',{
-            ablums:allPicArr
+        res.render('upload', {
+            ablums: allPicArr
         })
     })
 }
 // POST方法
-exports.doPost = function(req,res,next) {
-        var form = new formidable.IncomingForm();
-        form.uploadDir = path.normalize(__dirname + '/../backups/') 
-        form.parse(req, function(err,fields,files) {
-            if(err){
-                next();
+exports.doPost = function (req, res, next) {
+    var form = new formidable.IncomingForm();
+    form.uploadDir = path.normalize(__dirname + '/../backups/')
+    form.parse(req, function (err, fields, files) {
+        if (err) {
+            next();
+            return;
+        }
+        var wenjianjia = fields.wenjianjia;
+        var extname = path.extname(files.tupian.name)
+        var oldpath = files.tupian.path;
+        var newpath = path.normalize(__dirname + '/../uploads/' + wenjianjia + '/' + new Date().getTime() + extname)
+        fs.rename(oldpath, newpath, function (err) {
+            if (err) {
+                next()
+                return
+            }
+        })
+        // 文件上传成功之后，返回主页面
+        fileModel.getPicsFolder(function (err, allPicArr) {
+            if (err) {
+                // res.render('err');
+                next()
                 return;
             }
-            var wenjianjia = fields.wenjianjia;
-            var extname = path.extname(files.tupian.name)
-            var oldpath = files.tupian.path;
-            var newpath = path.normalize(__dirname + '/../uploads/' + wenjianjia + '/' + new Date().getTime() + extname)
-            fs.rename(oldpath,newpath,function(err){
-                if(err){
-                    next()
-                    return 
-                }
-            })
-            fileModel.getPicsFolder(function(err,allPicArr){
-                if(err){
-                    // res.render('err');
-                    next()
-                    return;
-                }
-                res.render('index',{
-                    ablums:allPicArr
+            var allPicsData = [];
+            fileModel.perFolderHas(allPicArr, function (isHasHolder) {
+                allPicsData = isHasHolder;
+                res.render('index', {
+                    ablums: allPicsData
                 })
-            })
+            });
+        })
     })
 }
--- src/common/node/nodedemo/models/file.js
+++ src/common/node/nodedemo/models/file.js
@@ -1,22 +1,23 @@
 var fs = require("fs");
 
-exports.getPicsFolder = function(callback) {
-    fs.readdir("./uploads",function(err,files){
-        if(err){
-            callback('么有找到UPLOADS的文件路径',null)
+// 获取uploads下所有的文件夹信息
+exports.getPicsFolder = function (callback) {
+    fs.readdir("./uploads", function (err, files) {
+        if (err) {
+            callback('么有找到UPLOADS的文件路径', null)
         }
         var picArr = [];
 
-        (function iterator(i){
-            if(i == files.length){
-                callback(null,picArr);
+        (function iterator(i) {
+            if (i == files.length) {
+                callback(null, picArr);
                 return;
             }
-            fs.stat('./uploads/' + files[i],function(err,stats){
-                if(err){
-                    callback("找不到"+file[i]+"这张图片!",null)
+            fs.stat('./uploads/' + files[i], function (err, stats) {
+                if (err) {
+                    callback("找不到" + file[i] + "这张图片!", null)
                 }
-                if(stats.isDirectory()){
+                if (stats.isDirectory()) {
                     picArr.push(files[i])
                 }
                 iterator(i + 1);
@@ -24,29 +25,53 @@ exports.getPicsFolder = function(callback) {
         })(0);
     })
 }
-exports.getAllPics = function(albumsNames,callback) {
-    fs.readdir("./uploads/" + albumsNames,function(err,files){
-        if(err){
-            callback('么有找到'+albumsNames+'文件',null);
+
+// 获取对应文件夹下的文件信息
+exports.getAllPics = function (albumsNames, callback) {
+    fs.readdir("./uploads/" + albumsNames, function (err, files) {
+        if (err) {
+            callback('么有找到' + albumsNames + '文件', null);
             return;
         }
         var pics = [];
 
-        (function iterator(i){
-            if(i == files.length){
-                callback(null,pics);
+        (function iterator(i) {
+            if (i == files.length) {
+                callback(null, pics);
                 return;
             }
-            fs.stat('./uploads/' + albumsNames + '/' + files[i],function(err,stats){
-                if(err){
-                    callback("找不到"+file[i]+"这张图片!",null);
+            fs.stat('./uploads/' + albumsNames + '/' + files[i], function (err, stats) {
+                if (err) {
+                    callback("找不到" + file[i] + "这张图片!", null);
                     return;
                 }
-                if(stats.isFile()){
+                if (stats.isFile()) {
                     pics.push(files[i])
                 }
                 iterator(i + 1);
             })
         })(0);
     })
+}
+
+// 判断所有文件夹时候含有子文件
+exports.perFolderHas = function (holders, callback) {
+    var isHasHolder = [];
+    (function iterator(i) {
+        if (i == holders.length) {
+            callback(isHasHolder);
+            return;
+        }
+        fs.readdir('./uploads/' + holders[i], function (err, files) {
+            if (err) {
+                callback(null)
+                return
+            }
+            isHasHolder.push({
+                holder: holders[i],
+                holderLen: files.length
+            })
+            iterator(i + 1);
+        })
+    })(0);
 }
